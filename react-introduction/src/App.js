5b541ea10e71ac0bb87bf10c02adbc5c2f451986
PranjalBuilds <imcae22060@gmail.com>
2025-02-01 02:43:32 +0530

    tailwind setup + hello react!

---
+++ react-introduction/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-9xl'> Hello React </h1>
+    </div>
+  );
+}
+
+export default App;
