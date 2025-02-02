46862bc0daecb1b19965b14452d0ae770f6223ea
PranjalBuilds <imcae22060@gmail.com>
2025-02-02 18:24:43 +0530

    Props

--- react-introduction/src/Components/Demo.js
+++ react-introduction/src/Components/Demo.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
-const Demo = () => {
+const Demo = (props) => {
   return (
-    <div className='mx-8 my-4 border-gray-200 px-4 py-2 border-2 rounded-xl w-1/3'>
-        <h3 className='text-4xl'>This is a component.</h3>
-        <p className='text-gray-500'>this is normal dummy text</p>
+    <div className='mx-2 my-1 border-gray-200 px-4 py-4 border-2 rounded-xl w-80 flex flex-col h-96'>
+  
+  {/* Image Section */}
+  <div className='h-48 w-full overflow-hidden rounded-t-lg'>
+    <img src={"https://picsum.photos/900/1600"} alt={props.title} className='object-cover w-full h-full'/>
+  </div>
+
+  {/* Content Section */}
+  <div className='flex flex-col justify-between flex-grow mt-4'>
+    {/* Title and Price */}
+    <div className='flex justify-between items-center mb-2'>
+      <h4 className='text-lg font-bold truncate'>{props.title}</h4>
+      <p className='text-lg font-bold text-red-600'>$ {props.price}</p>
     </div>
-  )
+
+    {/* Description */}
+    <p className='text-gray-500 text-sm line-clamp-3 mb-4'>{props.description}</p>
+
+    {/* Button */}
+    <button className='w-full py-2 text-white font-semibold bg-red-500 rounded-lg hover:bg-red-600 transition'>
+      Buy Now
+    </button>
+
+  </div>
+</div>
+
+  ) 
 }
 
-export default Demo
+export default Demo;
--- react-introduction/src/index.js
+++ react-introduction/src/index.js
@@ -6,13 +6,14 @@ import Demo from './Components/Demo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    {/* <App /> */}
-    <Demo/>
-    <Demo/>
-    <Demo/>
-    <Demo/>
-  </>
+  <div className='flex flex-wrap justify-center gap-4 m-4'>
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+    <Demo title='Sample Title' price="199" description='Lorem ipsum dolor sit amet consectetur Assumenda veniam quidem reprehenderit commodi, earum hic.' />
+  </div>
 );
-
-
