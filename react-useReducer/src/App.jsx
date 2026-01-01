9585dd9cf6167cb5932c4b7e2da6475d02f15dd4
PranjalBuilds <imcae22060@gmail.com>
2026-01-01 23:58:56 +0530

    Add to cart (using useReducer)

--- react-useReducer/src/App.jsx
+++ react-useReducer/src/App.jsx
@@ -1,8 +1,30 @@
 import { useReducer, useState } from 'react'
 
 function reducer(state, action){
-  
+  console.log('State: ', state);
+  console.log('Action: ', action);
+
+  switch (action.type) {
+    case 'ADD_ITEM': {
+      const newItem = `item ${state.length + 1}`
+      return [...state, newItem];
+    }
+
+    case 'REMOVE_ITEM': {
+      return state.filter((_, i) => i !== action.index) ;
+    }
+      
+    case 'CLEAR_CART': {
+      return [];
+    }
+      
+    default: {
+      return alert('ERROR: SOMETHING WENT WRONG, TRY AGAIN!')
+    }
+
+  }
 }
+
 const App = () => {
   const container = { 
     margin: "50px 50px",
@@ -18,21 +40,11 @@ const App = () => {
   };
 
   
-  const [cart, setCart] = useState([])
-
-  const addItem = () => {
-    const newItem = `item ${cart.length + 1}`
-    setCart((cart) => [...cart, newItem])
-  };
-
-  const removeItem = (index) => {
-    setCart((cart) => cart.filter((_, i) => i !== index));
-  };
+  const [cart, dispatch] = useReducer(reducer, [])
 
   const clearCart = () => {
     setCart([]);
   };
-  
   return (
     <>
       <div style={container}>
@@ -42,15 +54,15 @@ const App = () => {
             cart.map((item, index) => (
               <li key={item}> {item}
               &nbsp; 
-                <button onClick={() => removeItem(index)} style={btns}>Remove</button>
+                <button onClick={() => dispatch({type: 'REMOVE_ITEM', index: index})} style={btns}>Remove</button>
               </li>
             
           ))
         }
           </ul>
-        <button onClick={addItem}>Add item</button>
+        <button onClick={() => dispatch({type: 'ADD_ITEM'})}>Add item</button>
         &nbsp;
-        <button onClick={clearCart}>Clear cart</button>
+        <button onClick={() => dispatch({type : 'CLEAR_CART'})}>Clear cart</button>
         <p>Total items: {cart.length}</p>
       </div>
   </>
